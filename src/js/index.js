baaaa75a1a266c9c301f93b153d3072e68bf132a
javi-mera <fcojmera@gmail.com>
2020-07-05 19:15:25 +0000

    da error

--- src/js/index.js
+++ src/js/index.js
@@ -6,5 +6,24 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("Hello Rigo from the console!");
+  console.log("www." + pronoun, adj, noun, ext);
 };
+let pronoun = ["la", "el", "las", "los"];
+let adj = ["gran", "super", "enormes", "pequeno"];
+let noun = ["lugar", "academia", "agujero", "granja"];
+let ext = [".com", ".es", ".net", ".org"];
+
+function dominio(pronoun, adj, noun, ext) {
+  for (i = 0; i < pronoun.length; i++) {
+    for (j = 0; j < adj.length; j++) {
+      for (k = 0; k < noun.length; k++) {
+        for (l = 0; l < ext.length; l++) {
+          let frase = pronoun[i].concat(adj[j], noun[k], ext[l]);
+        }
+      }
+    }
+  }
+
+  document.getElementById("web");
+  return console.log(frase);
+}
