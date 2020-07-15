cb1c9bf94e9e832491abc7f6abafbd01bb939cfe
javi-mera <fcojmera@gmail.com>
2020-07-15 11:50:22 +0000

    ejercicio finalizado

--- src/js/index.js
+++ src/js/index.js
@@ -6,24 +6,26 @@ import "../assets/img/4geeks.ico";
 import "../style/index.scss";
 
 window.onload = function() {
-  console.log("www." + pronoun, adj, noun, ext);
+  let pronoun = ["la", "el", "las", "los"];
+  let adj = ["gran", "super", "enormes", "pequeno"];
+  let noun = ["lugar", "academia", "agujero", "granja"];
+  let ext = [".com", ".es", ".net", ".org"];
+
+  document.getElementById("web").innerHTML = dominio(pronoun, adj, noun, ext);
 };
-let pronoun = ["la", "el", "las", "los"];
-let adj = ["gran", "super", "enormes", "pequeno"];
-let noun = ["lugar", "academia", "agujero", "granja"];
-let ext = [".com", ".es", ".net", ".org"];
 
 function dominio(pronoun, adj, noun, ext) {
-  for (i = 0; i < pronoun.length; i++) {
-    for (j = 0; j < adj.length; j++) {
-      for (k = 0; k < noun.length; k++) {
-        for (l = 0; l < ext.length; l++) {
-          let frase = pronoun[i].concat(adj[j], noun[k], ext[l]);
+  let frase = [];
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < ext.length; l++) {
+          frase.push(
+            "<p>" + pronoun[i].concat(adj[j], noun[k], ext[l]) + "</p>"
+          );
         }
       }
     }
   }
-
-  document.getElementById("web");
-  return console.log(frase);
+  return frase;
 }
